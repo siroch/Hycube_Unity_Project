8881b5976d91b44a8042bf41106ddc4ddca2488d
Unknown <habizero@gmail.com>
2019-05-26 14:42:29 +0900

    refs #2 : "start_test"

--- Puzzle_Game/Assets/Scripts/start.cs
+++ Puzzle_Game/Assets/Scripts/start.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class start : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void destroy_me()
     {
-        
+        Destroy(this.gameObject);
     }
 
     // Update is called once per frame
-    void Update()
+    public void start_Game()
     {
-        
+        SceneManager.LoadScene("Selcet_Stage");
     }
 }
