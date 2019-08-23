b46d7b6075846c3fa43ca1b1cd69f0c57cb85f3d
Pjh <jiho5993@naver.com>
2019-08-23 20:18:17 +0900

    Add "Go Easy Stage" function

--- Puzzle_Game/Assets/Scripts/SceneChanger.cs
+++ Puzzle_Game/Assets/Scripts/SceneChanger.cs
@@ -5,27 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    public void GoSelectDifficulty()
+    public static bool Difficulty; // easy면 false, hard면 true
+
+    public void GoSelectDifficulty() // 난이도 설정창 들어가는 함수
     {
         SceneManager.LoadScene("SelectDifficulty");
     }
 
-    public void GoEasyStages()
+    public void GoEasyStages() // 쉬움 난이도를 선택했을 때
     {
         SceneManager.LoadScene("EasyStages");
+        Difficulty = false;
     }
 
-    public void GoHardStages()
+    public void GoHardStages() // 어려움 난이도를 선택했을 때
     {
         SceneManager.LoadScene("HardStages");
+        Difficulty = true;
     }
 
-    public void GoMainMenu()
+    public void GoMainMenu() // 메인메뉴로 돌아가는 함수
     {
         SceneManager.LoadScene("MainMenu");
     }
 
-    public void game_quit()
+    public void BackStage()
+    {
+        if(!Difficulty)
+        {
+            SceneManager.LoadScene("EasyStages");
+        }
+        else
+        {
+            SceneManager.LoadScene("HardStages");
+        }
+    }
+
+    public void game_quit() // 게임 종료
     {
         Application.Quit();
     }
